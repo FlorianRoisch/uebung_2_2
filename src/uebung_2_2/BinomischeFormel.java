e1945ad140ffb05338f5c5bbb6eaa0076e1ff4ab
14883 <14883@A101-16.nordakademie.intern>
2016-10-21 11:30:29 +0200

    Binomische Formel

---
+++ src/uebung_2_2/BinomischeFormel.java
@@ -0,0 +1,18 @@
+package uebung_2_2;
+
+public class BinomischeFormel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double a = 8;
+double b = 3;
+
+double loesung1 = (a + b)*(a + b);
+double loesung2 = (a * a) +2*a*b+ (b * b);
+
+System.out.println("Loesung =" + loesung1);
+System.out.println("Loesung =" + loesung2);
+
+	}
+
+}
